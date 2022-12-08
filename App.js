f51c09fb324d9b1b37f9260aae075f7fc691e971
Freewind7 <jenek777@ukr.net>
2022-12-08 13:42:22 +0100

    new paper

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>Будь ласка введіть свої облікові данні</Text>
+        <TextInput style={styles.input} keyboardType = "phone-pad"  placeholder ="Номер телефону"/>
+        <TextInput style={styles.input} secureTextEntry = "true" placeholder ="Пароль" />
+      </View>
+    );
+  }
 }
 
+
+// Стилі
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  //Основний контейнер 
+  container : {
+    backgroundColor : "lightblue",
+    position : "absolute",
+    width : "100%",
+    height : "100%", 
+  },
+  // Текст над полями
+  description : {
+    position : "absolute",
+    top : "35%",
+    left : "20%",
+  },
+  //Поля вводу
+  input: {
+  backgroundColor: "white",
+  width :"50%",
+  top : "40%",
+  left : "25%",
+  borderWidth : 1,
+  borderRadius : 10,
+  padding : 10,
+  margin : 10,
   },
 });
+
+// Функції
+
+
